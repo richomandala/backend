291706bf3f6e2f3c64f5b5d8c826e25f599781bb
ahmadsaifm <ahmad.saifm95@gmail.com>
2021-06-03 17:46:14 +0700

    set registrasi verifikasi login

--- middleware/index.js
+++ middleware/index.js
@@ -7,12 +7,12 @@ var verifikasi = require('./verifikasi')
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
-router.post('/api/v1/ubahpassword', verifikasi(1), auth.ubahPassword);
+router.post('/api/v1/ubahpassword', verifikasi(), auth.ubahPassword);
 
 router.get('/verify', auth.verifikasi)
 
 //alamat yang perlu otorisasi
 //halaman menampilkan data tabel oleh administrator
-router.get('/api/v1/admin/mahasiswa', verifikasi(1), auth.adminmahasiswa);
+router.get('/api/v1/admin/users', verifikasi(), auth.admin);
 
 module.exports = router;
